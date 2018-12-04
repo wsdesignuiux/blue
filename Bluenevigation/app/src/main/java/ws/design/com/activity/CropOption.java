d0bc3f9b0eeaab2bfa205f500e83165e527901c5
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-04 10:59:20 +0530

    Initial commit

---
+++ Bluenevigation/app/src/main/java/ws/design/com/activity/CropOption.java
@@ -0,0 +1,10 @@
+package ws.design.com.activity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+public class CropOption {
+	public CharSequence title;
+	public Drawable icon;
+	public Intent appIntent;
+}
---
+++ Bluenevigation/app/src/main/java/ws/design/com/activity/MainActivity.java
@@ -0,0 +1,289 @@
+package ws.design.com.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import ws.design.com.R;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    private Uri mImageCaptureUri;
+   // private ImageView mImageView;
+   ImageView banar1;
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int CROP_FROM_CAMERA = 2;
+    private static final int PICK_FROM_FILE = 3;
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] web = MainActivity.this.getResources().getStringArray(R.array.nav_drawer_labels);
+
+
+        Integer[] imageId = {
+                R.drawable.ic_home,
+                R.drawable.ic_tv,
+                R.drawable.ic_ticket,
+                R.drawable.ic_offers,
+                R.drawable.ic_earning,
+                R.drawable.ic_usemoney,
+                R.drawable.ic_ranking,
+                R.drawable.ic_faq,
+                R.drawable.ic_symbol21,
+
+
+        };
+
+
+
+        CustomList adapter1 = new
+                CustomList(MainActivity.this, web, imageId);
+        ListView list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter1);
+
+
+
+
+        final String [] items			= new String [] {"Take from camera", "Select from gallery"};
+        ArrayAdapter<String> adapter	= new ArrayAdapter<String> (this, android.R.layout.select_dialog_item,items);
+        AlertDialog.Builder builder		= new AlertDialog.Builder(this);
+
+        builder.setTitle("Select Image");
+        builder.setAdapter( adapter, new DialogInterface.OnClickListener() {
+            public void onClick( DialogInterface dialog, int item ) { //pick from camera
+                if (item == 0) {
+                    Intent intent 	 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+                            "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+
+                    try {
+                        intent.putExtra("return-data", true);
+
+                        startActivityForResult(intent, PICK_FROM_CAMERA);
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                } else { //pick from file
+                    Intent intent = new Intent();
+
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+                }
+            }
+        } );
+
+        final AlertDialog dialog = builder.create();
+       banar1=(ImageView)findViewById(R.id.banar1);
+        banar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                dialog.show();
+
+            }
+        });
+
+
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+
+        switch (requestCode) {
+            case PICK_FROM_CAMERA:
+                doCrop();
+
+                break;
+
+            case PICK_FROM_FILE:
+                mImageCaptureUri = data.getData();
+
+                doCrop();
+
+                break;
+
+            case CROP_FROM_CAMERA:
+                Bundle extras = data.getExtras();
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+
+                    banar1.setImageBitmap(photo);
+                }
+
+                File f = new File(mImageCaptureUri.getPath());
+
+                if (f.exists()) f.delete();
+
+                break;
+
+        }
+    }
+
+    private void doCrop() {
+        final ArrayList<CropOption> cropOptions = new ArrayList<CropOption>();
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setType("image/*");
+
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities( intent, 0 );
+
+        int size = list.size();
+
+        if (size == 0) {
+            Toast.makeText(this, "Can not find image crop app", Toast.LENGTH_SHORT).show();
+
+            return;
+        } else {
+            intent.setData(mImageCaptureUri);
+
+            intent.putExtra("outputX", 200);
+            intent.putExtra("outputY", 200);
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", true);
+
+            if (size == 1) {
+                Intent i 		= new Intent(intent);
+                ResolveInfo res	= list.get(0);
+
+                i.setComponent( new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+
+                startActivityForResult(i, CROP_FROM_CAMERA);
+            } else {
+                for (ResolveInfo res : list) {
+                    final CropOption co = new CropOption();
+
+                    co.title 	= getPackageManager().getApplicationLabel(res.activityInfo.applicationInfo);
+                    co.icon		= getPackageManager().getApplicationIcon(res.activityInfo.applicationInfo);
+                    co.appIntent= new Intent(intent);
+
+                    co.appIntent.setComponent( new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+
+                    cropOptions.add(co);
+                }
+
+                CropOptionAdapter adapter = new CropOptionAdapter(getApplicationContext(), cropOptions);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose Crop App");
+                builder.setAdapter( adapter, new DialogInterface.OnClickListener() {
+                    public void onClick( DialogInterface dialog, int item ) {
+                        startActivityForResult( cropOptions.get(item).appIntent, CROP_FROM_CAMERA);
+                    }
+                });
+
+                builder.setOnCancelListener( new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel( DialogInterface dialog ) {
+
+                        if (mImageCaptureUri != null ) {
+                            getContentResolver().delete(mImageCaptureUri, null, null );
+                            mImageCaptureUri = null;
+                        }
+                    }
+                } );
+
+                AlertDialog alert = builder.create();
+
+                alert.show();
+            }
+        }
+    }
+    public class CustomList extends ArrayAdapter<String> {
+
+        private final Activity context;
+        private final String[] web;
+        private final Integer[] imageId;
+        public CustomList(Activity context,
+                          String[] web, Integer[] imageId) {
+            super(context, R.layout.nav_drawer_row, web);
+            this.context = context;
+            this.web = web;
+            this.imageId = imageId;
+
+        }
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater = context.getLayoutInflater();
+
+
+
+            View rowView= inflater.inflate(R.layout.nav_drawer_row, null, true);
+            TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+
+
+
+
+
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+            txtTitle.setText(web[position]);
+
+            imageView.setImageResource(imageId[position]);
+            return rowView;
+        }
+    }
+    /*@Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }*/
+}
